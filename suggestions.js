3a24ac22ef9a3bcd73aad53cc58b35279d51f3aa
Carwin Young <carwinyoung@gmail.com>
2013-07-04 01:14:10 -0500

    Defined suggestion paths.

--- suggestions.js
+++ suggestions.js
@@ -1,67 +1,66 @@
 var compassSuggestions = {
-  'CSS3': 
+  'CSS3':
     [
-      'Appearance',
-      'Background Clip',
-      'Background Origin',
-      'Background Size',
-      'Border Radius',
-      'Box',
-      'Box Shadow',
-      'Box Sizing',
-      'CSS Regions',
-      'CSS3 Pie',
-      'Columns',
-      'Filter',
-      'Font Face',
-      'Hyphenation',
-      'Images',
-      'Inline Block',
-      'Opacity',
-      'Shared Utilities',
-      'Text Shadow',
-      'Transform',
-      'Transition',
-      'User Interface'
+      { 'name': 'Appearance', 'path': 'appearance' },
+      { 'name': 'Background Clip', 'path': 'background_clip' },
+      { 'name': 'Background Origin', 'path': 'background_origin' },
+      { 'name': 'Background Size', 'path': 'background_size' },
+      { 'name': 'Border Radius', 'path': 'border_radius' },
+      { 'name': 'Box', 'path': 'box' },
+      { 'name': 'Box Shadow', 'path': 'box_shadow' },
+      { 'name': 'Box Sizing', 'path': 'box_sizing' },
+      { 'name': 'CSS Regions', 'path': 'css_regions' },
+      { 'name': 'CSS3 Pie', 'path': 'pie' },
+      { 'name': 'Columns', 'path': 'columns' },
+      { 'name': 'Filter', 'path': 'filter' },
+      { 'name': 'Font Face', 'path': 'font_face' },
+      { 'name': 'Hyphenation', 'path': 'hyphenation' },
+      { 'name': 'Images', 'path': 'images' },
+      { 'name': 'Inline Block', 'path': 'inline_block' },
+      { 'name': 'Opacity', 'path': 'opacity' },
+      { 'name': 'Shared Utilities', 'path': 'shared' },
+      { 'name': 'Text Shadow', 'path': 'text-shadow' },
+      { 'name': 'Transform', 'path': 'transform' },
+      { 'name': 'Transition', 'path': 'transition' },
+      { 'name': 'User Interface', 'path': 'user_interface' }
     ],
-  'Helpers': 
+  'Helpers':
     [
-      'Color Stops',
-      'Colors',
-      'Constants',
-      'Cross Browser',
-      'Display Helpers',
-      'Font Files',
-      'Image Dimensions',
-      'Inline Data',
-      'Math',
-      'Selectors',
-      'Sprites',
-      'URLs'
+      { 'name': 'Color Stops', 'path': 'color-stops' },
+      { 'name': 'Colors', 'path': 'colors' },
+      { 'name': 'Cross Browser', 'path': 'cross-browser' },
+      { 'name': 'Display Helpers', 'path': 'display' },
+      { 'name': 'Font Files', 'path': 'font-files' },
+      { 'name': 'Image Dimensions', 'path': 'image-dimensions' },
+      { 'name': 'Inline Data', 'path': 'inline-data' },
+      { 'name': 'Math', 'path': 'math' },
+      { 'name': 'Selectors', 'path': 'selectors' },
+      { 'name': 'Sprites', 'path': 'sprites' },
+      { 'name': 'URLs', 'path': 'urls' }
     ],
-  'Layout': 
+  'Layout':
     [
-      'Grid Background',
-      'Sticky Footer',
-      'Stretching'
+      { 'name': 'Grid Background', 'path': 'grid_background' },
+      { 'name': 'Sticky Footer', 'path': 'sticky_footer' },
+      { 'name': 'Stretching', 'path': 'stretching' }
     ],
-  'Reset': 
+  'Reset':
     [
-      'Reset Utilities'
+      { 'name': 'Reset Utilities', 'path': 'utilities' }
     ],
-  'Typography': 
+  'Typography':
     [
-      'Links',
-      'Lists',
-      'Text',
-      'Vertical Rhythm'
+      { 'name': 'Links', 'path': 'links' },
+      { 'name': 'Lists', 'path': 'lists' },
+      { 'name': 'Text', 'path': 'text' },
+      { 'name': 'Vertical Rhythm', 'path': 'vertical_rhythm' }
     ],
-  'Utilities': 
+  'Utilities':
     [
-      'Color',
-      'General',
-      'Print',
-      'Sprites',
-      'Tables'
+      { 'name': 'Color', 'path': 'color' },
+      { 'name': 'General', 'path': 'general' },
+      { 'name': 'Print', 'path': 'print' },
+      { 'name': 'Sprites', 'path': 'sprites' },
+      { 'name': 'Tables', 'path': 'tables' }
     ],
 };
